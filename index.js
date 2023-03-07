beb5c34591cfd485ad359a1c0f9fe5dadb4009f5
+ <+>
2023-03-07 12:24:13 -0500

    uploading intro to functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Follow along with the examples here
+ 
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+  
